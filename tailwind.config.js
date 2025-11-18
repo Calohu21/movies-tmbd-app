8f579814f02d641e166107e79f2512effe8e1729
Carmelo <carmelopola@hotmail.com>
2025-11-18 17:43:41 +0100

    Refactor project structure and improve styling with Tailwind CSS and DaisyUI integration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['night'],
+  },
+};
