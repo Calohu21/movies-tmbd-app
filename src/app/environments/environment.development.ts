49881d8c1b0b942decbc0ded06b4adb10424b400
Carmelo <carmelopola@hotmail.com>
2025-11-19 13:13:07 +0100

    feat(services): add movies.service.ts and method getTrendingMovies

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  tmdbApiKey: '354a2af82d27c97232c75afbd0f7c474',
+  tmdbUrl: 'https://api.themoviedb.org/3',
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiKey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTRhMmFmODJkMjdjOTcyMzJjNzVhZmJkMGY3YzQ3NCIsIm5iZiI6MTc2MjM0MjUwNy43MTQsInN1YiI6IjY5MGIzNjZiY2E4NTFjMTMyZWUxODUwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oPHfofBAsDXI7ZRnwCwYAimJpm79UzQMfx2I1vcw27A',
+  apiUrl: 'https://api.themoviedb.org/3',
+};
