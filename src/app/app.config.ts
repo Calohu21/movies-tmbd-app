797d28517d337e9f3924de29e2e6ee222ddf4817
Carmelo <carmelopola@hotmail.com>
2025-11-19 19:47:37 +0100

    feat(interceptors): add language.interceptor.ts and import in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,14 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { languageInterceptor } from './core/interceptors/language.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([languageInterceptor])),
   ],
 };
