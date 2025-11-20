22315e371c39006edc79068d4cd07671477eb766
Carmelo <carmelopola@hotmail.com>
2025-11-20 22:31:36 +0100

    feat(movie-trailer): implement movie trailer functionality and update interceptor logic

--- src/app/core/interceptors/language.interceptor.ts
+++ src/app/core/interceptors/language.interceptor.ts
@@ -2,10 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 export const languageInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.url.includes(environment.apiUrl)) {
+  if (req.url.includes(environment.apiUrl) && !req.url.includes('/videos')) {
     const modifiedReq = req.clone({
       setParams: {
         language: 'es-ES',
+        region: 'ES',
       },
     });
 
--- src/app/movies/pages/home-page/components/hero/hero.ts
+++ src/app/movies/pages/home-page/components/hero/hero.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MoviesService } from '../../../../movies.service';
+import { Video } from '../../../../../core/models/video.interface';
 
 @Component({
   selector: 'app-hero',
@@ -7,5 +8,31 @@ import { MoviesService } from '../../../../movies.service';
   templateUrl: './hero.html',
 })
 export class Hero {
-  trendingMoviesResource = inject(MoviesService).trendingMoviesResource;
+  private readonly movieService = inject(MoviesService);
+  readonly trendingMoviesResource = this.movieService.trendingMoviesResource;
+  readonly trailerKey = signal<string | null>(null);
+
+  getTrailer(movieId: number) {
+    this.movieService.getMovieVideos(movieId).subscribe({
+      next: (videos: Video[]) => {
+        console.log({ videos });
+        const oficialTrailer = this.findOfficialTrailer(videos);
+        console.log('Oficial trailer', oficialTrailer);
+        if (oficialTrailer) this.trailerKey.set(oficialTrailer.key);
+        console.log('TrailerKey: ', this.trailerKey());
+      },
+      error: (err) => console.error('ERROR al obtener videos:', err),
+    });
+  }
+
+  findOfficialTrailer(videos: Video[]): Video | undefined {
+    let trailer = videos.find((v) => v.type === 'Trailer' && v.official && v.site === 'YouTube');
+    if (!trailer) {
+      trailer = videos.find((v) => v.type === 'Trailer' && v.site === 'YouTube');
+    }
+    if (!trailer) {
+      trailer = videos.find((v) => v.type === 'Teaser' && v.official && v.site === 'YouTube');
+    }
+    return trailer;
+  }
 }
