332287b1b51353fafff89b01dd76ef96b7ad1de1
Carmelo <carmelopola@hotmail.com>
2025-11-20 23:51:34 +0100

    feat(hero): randomize movie selection and conditionally display trailer button

--- src/app/movies/pages/home-page/components/hero/hero.ts
+++ src/app/movies/pages/home-page/components/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { MoviesService } from '../../../../movies.service';
 import { Video } from '../../../../../core/models/video.interface';
 
@@ -9,17 +9,25 @@ import { Video } from '../../../../../core/models/video.interface';
 })
 export class Hero {
   private readonly movieService = inject(MoviesService);
+  private readonly trailerKey = signal<string | null>(null);
+  readonly hasTrailerKey = computed(() => this.trailerKey() !== null);
   readonly trendingMoviesResource = this.movieService.trendingMoviesResource;
-  readonly trailerKey = signal<string | null>(null);
+  readonly random = signal(0);
+  constructor() {
+    effect(() => {
+      this.random.set(Math.floor(Math.random() * 20));
+    });
+  }
 
   getTrailer(movieId: number) {
     this.movieService.getMovieVideos(movieId).subscribe({
       next: (videos: Video[]) => {
-        console.log({ videos });
         const oficialTrailer = this.findOfficialTrailer(videos);
-        console.log('Oficial trailer', oficialTrailer);
-        if (oficialTrailer) this.trailerKey.set(oficialTrailer.key);
-        console.log('TrailerKey: ', this.trailerKey());
+        if (oficialTrailer) {
+          this.trailerKey.set(oficialTrailer.key);
+        } else {
+          this.trailerKey.set(null);
+        }
       },
       error: (err) => console.error('ERROR al obtener videos:', err),
     });
