b497921a57da3aefe90d268b98df0044b78bfca0
Carmelo <carmelopola@hotmail.com>
2025-11-21 20:05:41 +0100

    feat(hero): implement movie carousel with autoplay and trailer functionality

--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { map, catchError, switchMap } from 'rxjs/operators';
 
 import { environment } from '../environments/environment';
@@ -26,28 +26,29 @@ export class MoviesService {
       .pipe(map((response) => response.results));
   }
 
-  getRandomTrendingMovieWithTrailer(): Observable<MovieWithTrailer> {
+  getTrendingMovieWithTrailer(): Observable<MovieWithTrailer[]> {
     return this.getTrendingMovies().pipe(
       switchMap((movies) => {
         if (!movies || movies.length === 0) {
           throw new Error('No trending movies found');
         }
 
-        const randomIndex = Math.floor(Math.random() * movies.length);
-        const movie = movies[randomIndex];
-
-        return this.getMovieVideos(movie.id).pipe(
-          map((videos) => ({
-            movie,
-            trailerKey: this.findOfficialTrailerKey(videos),
-          })),
-          catchError(() => of({ movie, trailerKey: null })),
+        const movieWithTrailerRequests: Observable<MovieWithTrailer>[] = movies.map((movie) =>
+          this.getMovieVideos(movie.id).pipe(
+            map((videos) => ({
+              movie,
+              trailerKey: this.findOfficialTrailerKey(videos),
+            })),
+            catchError(() => of({ movie, trailerKey: null })),
+          ),
         );
+
+        return forkJoin(movieWithTrailerRequests);
       }),
     );
   }
 
-  findOfficialTrailerKey(videos: Video[]): string | null {
+  private findOfficialTrailerKey(videos: Video[]): string | null {
     const trailer =
       videos.find(
         (v) => v.type === 'Trailer' && v.official && v.site === 'YouTube' && v.iso_639_1 === 'es',
