fbef1952a251b0c751f4c531ab0900081c3d43f5
Carmelo <carmelopola@hotmail.com>
2025-11-24 20:51:44 +0100

    refactor(home-page): remove layout component and integrate its content directly into home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './movies/pages/home-page/home-page';
-import { Layout } from './movies/pages/home-page/layout/layout';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Layout,
-    children: [
-      {
-        path: '',
-        component: HomePage,
-      },
-    ],
+    component: HomePage,
   },
   {
     path: '**',
--- src/app/movies/pages/home-page/home-page.ts
+++ src/app/movies/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Hero } from './components/hero/hero';
+import { RouterOutlet } from '@angular/router';
+import { Upcoming } from './components/upcoming/upcoming';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [Hero, RouterOutlet, Upcoming],
   templateUrl: './home-page.html',
   styles: ``,
 })
