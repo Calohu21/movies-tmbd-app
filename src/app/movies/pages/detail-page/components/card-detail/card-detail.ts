26555746449ab2f1669f1957eec7ec706cccc649
Carmelo <carmelopola@hotmail.com>
2025-11-25 17:00:13 +0100

    feat(shared): add `RuntimePipe` for converting runtime in minutes to hours and minutes format and integrate with detail page

--- src/app/movies/pages/detail-page/components/card-detail/card-detail.ts
+++ src/app/movies/pages/detail-page/components/card-detail/card-detail.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { DatePipe, NgOptimizedImage } from '@angular/common';
 import { DetailMovie } from '../../../../../core/models/movie.detail.interface';
+import { RuntimePipe } from '../../../../../shared/pipes/runtime.pipe';
 
 @Component({
   selector: 'app-card-detail',
-  imports: [NgOptimizedImage, DatePipe],
+  imports: [NgOptimizedImage, DatePipe, RuntimePipe, RuntimePipe],
   templateUrl: './card-detail.html',
   styles: ``,
 })
